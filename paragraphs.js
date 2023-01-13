5238ed629b3f55cf671994d88157c9066cf1d6c1
Solomon Great <greatsoma2019@gmail.com>
2023-01-13 12:01:36 +0100

    Create paragraphs.js

---
+++ paragraphs.js
@@ -0,0 +1,3 @@
+const paragraphs = [
+    "I started developing software more than ten years ago. Since then, I've built many applications, created dozens of open source projects, and pushed thousands of commits. Besides that, I spoke in more than a hundred conferences, and had the opportunity to chat with a ton of developers along the way.",
+]
