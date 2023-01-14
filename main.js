e8c6c75cdab824b5890230b6be9a703db8fd7176
Solomon Great <greatsoma2019@gmail.com>
2023-01-14 18:22:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,27 +22,6 @@ function randomParagraph() {
 }
 randomParagraph();
 
-
-inputField.oninput=()=>{
-    let typedChar = inputField.value.split("")[charIndex]
-    const characters = typingText.querySelectorAll('span')
-    if(!isTyping){
-        timer = setInterval(()=>{
-            if(timeleft>0){
-                timeleft--;
-                timeTag.textContent=timeleft;
-            }else{
-                clearInterval (timer)
-            }
-            
-        },1000)
-        isTyping=true;
-    }
-    
-    if(typedChar == null){
-        charIndex--
-        if(characters[charIndex].classList.contains('wrong')){
-            mistakes--;
         }
         characters[charIndex].classList.remove('wrong','correct')
         characters[charIndex+1].classList.remove('active')
